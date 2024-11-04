2105229c9640402f50c490ad9fd5521210f1c118
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-11-04 19:08:47 -0400

    significant improvement on data processing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.data.data_process import cleanData
 
 def main():
-    cleanData(decode_path="data/external/decode.json").group_by_naics_code()
+    cleanData(decode_path="data/external/decode.json").make_dataset()
 
 if __name__ == "__main__":
     main()
