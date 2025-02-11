f4fd21871cd64621e1259e912ee7102382c69b64
BryanDVG <70717360+BryanDVG@users.noreply.github.com>
2025-02-11 15:09:12 -0400

    18 graph function (#26)
    * #18 Created a function that returns a graph of a naics code
    
    * #18 Changed version number

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from src.data.data_process import cleanData
+from src.visualization.graph import graphGenerator
 
 
 def main():
-    cleanData().make_qcew_dataset()
+    # cleanData().make_qcew_dataset()
     # print(cleanData().group_by_naics_code().to_pandas())
     # print(cleanData().unique_naics_code().to_pandas())
+    graphGenerator().create_graph("5412")
 
 
 if __name__ == "__main__":
