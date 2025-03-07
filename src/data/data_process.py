8f068431b7b296f11ad5432a2f139baec231b54a
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-03-07 20:50:43 +0000

    27 insert (#28)
    * remove deprecated code
    
    * update insert to use duckdb
    
    * remove unused importws

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -1,12 +1,10 @@
-import ibis.expr.types as it
 import geopandas as gpd
-from ..models import *
+from ..models import init_qcew_table, get_conn
 from tqdm import tqdm
 import polars as pl
 import pandas as pd
 import requests
 import logging
-import ibis
 import json
 import os
 
@@ -15,18 +13,18 @@ class cleanData:
     def __init__(
         self,
         saving_dir: str = "data/",
-        database_url: str = "duckdb:///data.ddb",
+        database_file: str = "data.ddb",
+        log_file: str = "data_process.log",
     ):
-        self.database_url = database_url
         self.saving_dir = saving_dir
-        self.data_file = self.database_url.split("///")[1]
-        self.conn = ibis.duckdb.connect(f"{self.data_file}")
+        self.data_file = database_file
+        self.conn = get_conn(self.data_file)
 
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s - %(levelname)s - %(message)s",
             datefmt="%d-%b-%y %H:%M:%S",
-            filename="data_process.log",
+            filename=log_file,
         )
         # Check if the saving directory exists
         if not os.path.exists(self.saving_dir + "raw"):
@@ -35,19 +33,13 @@ class cleanData:
             os.makedirs(self.saving_dir + "processed")
         if not os.path.exists(self.saving_dir + "external"):
             os.makedirs(self.saving_dir + "external")
-
         if not os.path.exists(self.saving_dir + "external/decode.json"):
             self.pull_file(
                 url="https://github.com/ouslan/jp-QCEW/tree/main/data/external",
                 filename=f"{self.saving_dir}external/decode.json",
             )
 
-        if "qcewtable" not in self.conn.list_tables():
-            init_qcew_table(self.data_file)
-        if "hactable" not in self.conn.list_tables():
-            init_hac_table(self.data_file)
-
-    def make_qcew_dataset(self) -> None:
+    def make_qcew_dataset(self) -> pl.DataFrame:
         """
         This function reads the raw data files in data/raw and inserts them into the database.
 
@@ -59,25 +51,32 @@ class cleanData:
         -------
         None
         """
-        for folder in os.listdir(f"{self.saving_dir}/raw"):
-            count = 0
-            if folder == ".gitkeep" or folder == ".DS_Store":
-                continue
-            else:
-                for file in os.listdir(f"{self.saving_dir}/raw/{folder}"):
-                    df = self.clean_txt(
-                        f"{self.saving_dir}raw/{folder}/{file}",
-                        f"{self.saving_dir}external/decode.json",
-                    )
-                    if df.empty:
-                        logging.warning(f"File {file} is empty.")
-                        continue
-                    else:
-                        self.conn.insert("qcewtable", df)
-                        logging.info(
-                            f"File {file} for {folder} has been inserted into the database."
+        if "qcewtable" not in self.conn.sql("SHOW TABLES;").df().get("name").tolist():
+            init_qcew_table(self.data_file)
+            for folder in os.listdir(f"{self.saving_dir}/raw"):
+                count = 0
+                if folder == ".gitkeep" or folder == ".DS_Store":
+                    continue
+                else:
+                    for file in os.listdir(f"{self.saving_dir}/raw/{folder}"):
+                        df = self.clean_txt(
+                            f"{self.saving_dir}raw/{folder}/{file}",
+                            f"{self.saving_dir}external/decode.json",
                         )
-                        count += 1
+                        if df.empty:
+                            logging.warning(f"File {file} is empty.")
+                            continue
+                        else:
+                            self.conn.sql(
+                                "INSERT INTO 'qcewtable' BY NAME SELECT * FROM df"
+                            )
+                            logging.info(
+                                f"File {file} for {folder} has been inserted into the database."
+                            )
+                            count += 1
+            return self.conn.sql("SELECT * FROM qcewtable").pl()
+        else:
+            return self.conn.sql("SELECT * FROM qcewtable").pl()
 
     def clean_txt(self, dev_file: str, decode_path: str) -> pd.DataFrame:
         """
@@ -140,7 +139,7 @@ class cleanData:
 
         return gdf
 
-    def group_by_naics_code(self) -> it.Table:
+    def group_by_naics_code(self) -> pl.DataFrame:
         """
         This function aggregate the data by year, quarter, and first 4 digits of the NAICS code.
 
@@ -152,37 +151,37 @@ class cleanData:
         -------
         it.Table
         """
-        df = self.conn.table("qcewtable")
+        df = self.conn.sql("SELECT * FROM qcewtable").pl()
 
-        df = df.mutate(
-            total_employment=(
-                df.first_month_employment
-                + df.second_month_employment
-                + df.third_month_employment
+        df = df.with_column(
+            total_wages=(
+                pl.col("first_month_employment")
+                + pl.col("second_month_employment")
+                + pl.col("third_month_employment")
             )
             / 3
         )
 
-        df = df.mutate(
-            first_4_naics_code=df.naics_code.substr(0, 4),
-            dummy=ibis.literal(1),
+        df = df.with_column(
+            naics4=pl.col("naics_code").str.slice(0, 4),
+            dummy=pl.lit(1),
         )
-        df = df.filter(df.first_4_naics_code != "")
+        df = df.filter(pl.col("naics4") != "")
 
         # Group by the specified columns and aggregate
-        df = df.group_by(["year", "qtr", "first_4_naics_code"]).aggregate(
-            total_wages_sum=df.total_wages.sum(),
-            total_employment_sum=df.total_employment.sum(),
-            dummy_sum=df.dummy.sum(),
+        df = df.group_by(["year", "qtr", "first_4_naics_code"]).agg(
+            total_wages_sum=pl.col("total_wages").sum(),
+            total_employment_sum=pl.col("total_employment").mean(),
+            dummy_sum=pl.col("dummy").sum(),
         )
 
-        df = df.filter(df["dummy_sum"] > 4)
+        df = df.filter(pl.col("dummy_sum") > 4)
 
         # Step 2: Add calculated columns for contributions
-        df = df.mutate(
-            fondo_contributions=df["total_wages_sum"] * 0.014,
-            medicare_contributions=df["total_wages_sum"] * 0.0145,
-            ssn_contributions=df["total_wages_sum"] * 0.062,
+        df = df.with_column(
+            fondo_contributions=pl.col("fondo_contributions") * 0.014,
+            medicare_contributions=pl.col("medicare_contributions") * 0.0145,
+            ssn_contributions=pl.col("ssn_contributions") * 0.062,
         )
 
         return df
