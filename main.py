82da73cb78f6079dbc93411c90a490dd0bc0c90d
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-03-07 16:49:12 -0400

    update insert to use duckdb

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.visualization.graph import graphGenerator
+from src.data.data_process import cleanData
 
 
 def main():
-    # cleanData().make_qcew_dataset()
+    return cleanData().make_qcew_dataset()
     # print(cleanData().group_by_naics_code().to_pandas())
     # print(cleanData().unique_naics_code().to_pandas())
-    graphGenerator().create_graph("5412")
+    # graphGenerator().create_graph("5412")
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
